92bdd45c92d0eff3cacdb38e4c43feddbad3b988
Ivan Martinez <martinezezequielivan@gmail.com>
2023-05-21 15:17:25 -0300

    Agregando peticion al endpoint de la API de pokemon y la lista con los modals

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { APIService } from '../services/api.service';
+import { IonModal } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core/components';
+
 
 @Component({
   selector: 'app-tabs',
@@ -7,6 +11,29 @@ import { Component } from '@angular/core';
 })
 export class TabsPage {
 
-  constructor() {}
+  pokemon: any[] = [];
+  @ViewChild(IonModal)
+  modal!: IonModal;
+
+
+  constructor(public api:APIService) {
+    // this.pokemon = api.getPokemon(1)
+    api.getPokemon(2).then(r =>{
+      this.pokemon[0] = r 
+    } )
+    api.getPokemon(5).then(r =>{
+      this.pokemon[1] = r 
+    } )
+    api.getPokemon(7).then(r =>{
+      this.pokemon[2] = r 
+    } )
+    api.getPokemon(10).then(r =>{
+      this.pokemon[3] = r 
+    } )
+  }
+
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
 
 }
