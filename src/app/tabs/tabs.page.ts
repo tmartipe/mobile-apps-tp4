a9e5a996d9ec3b506df887bd9ffc1d45eaed098b
Tomas Martinez <tomasurielmp@gmail.com>
2023-05-22 17:50:54 -0300

    feat: Refactoring code to follow good practices

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,11 +1,11 @@
-import { Component, ViewChild, getNgModuleById } from '@angular/core';
-import { APIService } from '../services/api.service';
-import { IonModal } from '@ionic/angular';
-import { OverlayEventDetail } from '@ionic/core/components';
-import { StorageService } from '../services/storage.service';
-import { Pokemon } from '../interfaces/pokemon';
+import {Component, ViewChild } from '@angular/core';
+import {APIService} from '../services/api.service';
+import {IonModal} from '@ionic/angular';
+import {StorageService} from '../services/storage.service';
+import {Pokemon, ApiPokemon} from '../interfaces/pokemon';
 
 
+// @ts-ignore
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
@@ -14,55 +14,52 @@ import { Pokemon } from '../interfaces/pokemon';
 export class TabsPage {
 
   pokeNumber: number = 0;
-  pokemon: any[] = [];
-  pokeList: Pokemon[] = [];
+  viewPokemonArray: Pokemon[] = [];
+  savedPokemon: Pokemon[] = [];
   @ViewChild(IonModal)
   modal!: IonModal;
-  savePoke: boolean = false;
 
-  constructor(public api:APIService, private storage:StorageService) {
-    // this.pokemon = api.getPokemon(1)
-    
-  }
+  constructor(public api: APIService, private storage: StorageService){}
 
   cancel() {
     this.modal.dismiss(null, 'cancel');
   }
 
-  save(){
-    if(!this.pokemon.find(p=>p.id == this.pokeNumber)) 
-    {
-      this.getStorage(this.pokeNumber)
+  save() {
+    if (!this.viewPokemonArray.find(p => p.id == this.pokeNumber)) {
+      this.findPokemonByNumber(this.pokeNumber)
     }
   }
-  //pokelist es la lista que se guiarda en el storage
 
-  setStorage(poke:any){
-    let pokemon:Pokemon={id:poke.id, name:poke.name ,front_default:poke.sprites.front_default, height:poke.height, weight:poke.weight}
-    this.pokeList.push(pokemon)
-    
-    let list = JSON.stringify(this.pokeList)
-    this.storage.update("pokemon", list)
+  savePokemonToStorage(apiPokemon: ApiPokemon){
+    let pokemon = this.pokemonFactory(apiPokemon);
+    this.savedPokemon.push(pokemon)
+    this.viewPokemonArray.push(pokemon)
+    let savedPokemonJSON = JSON.stringify(this.savedPokemon)
+    this.storage.update("pokemon", savedPokemonJSON)
   }
 
-  getStorage(number:any){
-    let datos
-    this.storage.read("pokemon").then((data =>{
-      if(data != undefined){
-        datos = JSON.parse(data.value);
-        datos = datos != null ? datos.find((p: Pokemon) => p.id == number): null;
-        console.log(datos)
-        if(datos == null){
-          this.api.getPokemon(number).then(r =>{
-            this.setStorage(r)
-            this.pokemon.push(r)
-          }) 
-        }else{
-        this.pokemon.push(datos)
-        }
+  findPokemonByNumber(pokeId: number) {
+    this.storage.read("pokemon").then((storageData => {
+      if (storageData.value != undefined) {
+        let savedPokes: Pokemon[] = JSON.parse(storageData.value!!);
+        let pokemon = savedPokes.find((p: Pokemon) => p.id == pokeId);
+        if (pokemon != undefined)
+          this.viewPokemonArray.push(pokemon)
       }
-      
+      this.api.getPokemon(pokeId).then(r=> {
+        this.savePokemonToStorage(r as ApiPokemon)
+      })
     }))
   }
- 
+  pokemonFactory(apiPokemon: ApiPokemon): Pokemon{
+    return {
+      id: apiPokemon.id,
+      name: apiPokemon.name,
+      sprite: apiPokemon.sprites.front_default,
+      height: apiPokemon.height,
+      weight: apiPokemon.weight
+    }
+  }
+
 }
