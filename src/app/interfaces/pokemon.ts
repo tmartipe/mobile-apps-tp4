23e8cca773b496eda2a201b7e840197a389eeb6f
MondinoPedro <mondinopedro12@gmail.com>
2023-05-22 12:44:16 -0300

    Plugin Preferences added with conflicts

---
+++ src/app/interfaces/pokemon.ts
@@ -0,0 +1,6 @@
+export interface Pokemon{
+    id: string,
+    image:string,
+    height:string,
+    weight:string,
+}
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,7 +1,9 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, getNgModuleById } from '@angular/core';
 import { APIService } from '../services/api.service';
 import { IonModal } from '@ionic/angular';
 import { OverlayEventDetail } from '@ionic/core/components';
+import { StorageService } from '../services/storage.service';
+import { Pokemon } from '../interfaces/pokemon';
 
 
 @Component({
@@ -11,29 +13,53 @@ import { OverlayEventDetail } from '@ionic/core/components';
 })
 export class TabsPage {
 
+  pokeNumber: number = 0;
   pokemon: any[] = [];
+  pokeList: Pokemon[] = [];
   @ViewChild(IonModal)
   modal!: IonModal;
+  savePoke: boolean = true;
 
-
-  constructor(public api:APIService) {
+  constructor(public api:APIService, private storage:StorageService) {
     // this.pokemon = api.getPokemon(1)
-    api.getPokemon(2).then(r =>{
-      this.pokemon[0] = r 
-    } )
-    api.getPokemon(5).then(r =>{
-      this.pokemon[1] = r 
-    } )
-    api.getPokemon(7).then(r =>{
-      this.pokemon[2] = r 
-    } )
-    api.getPokemon(10).then(r =>{
-      this.pokemon[3] = r 
-    } )
+    
   }
 
   cancel() {
     this.modal.dismiss(null, 'cancel');
   }
 
+  save(){
+    //falta condicion 
+    this.api.getPokemon(this.pokeNumber).then(r =>{
+      this.setStorage(r)
+    })
+   
+  }
+  //pokelist es la lista que se guiarda en el storage
+
+  setStorage(poke:any){
+    let pokemon:Pokemon={id:poke.id, image:poke.sprites.front_default, height:poke.height, weight:poke.weight}
+    this.pokeList.push(pokemon)
+    this.storage.update("pokemon", this.pokeList)
+  }
+
+  getStorage(number:any){
+    this.storage.read("pokemon").then((data=>{
+      if(data !== null){
+        data.value.forEach(p=>{
+          if(p.id === number){
+            this.savePoke = false
+            //Si esto pasa quiere decir que el pokemon ya existe dentro del storage, entonces no deberiamos pegarle a la api
+          }
+        })
+      }
+    }))
+    if(this.savePoke){
+      return true
+    }else{
+      return false
+    }
+  }
+ 
 }
